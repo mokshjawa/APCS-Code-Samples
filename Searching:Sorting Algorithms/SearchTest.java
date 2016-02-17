98dfd1b1fe0d1fa2143e07dd7eee2e9ef3c224a7
Moksh Jawa <mokshjawa@gmail.com>
2016-02-17 01:00:45 -0800

    Cleaning up code

--- Searching:Sorting Algorithms/SearchTest.java
+++ Searching:Sorting Algorithms/SearchTest.java
@@ -1,47 +1,42 @@
-// SearchTest.java
-
 public class SearchTest
 {
-  public static void main( String[] args )
+  public static void main(String[] args)
   {
-    int[] intArray = { 5, 6, 8, 2, 3 };
-    System.out.println( linearSearch(intArray, 2) );
+    int[] intArray = {5, 6, 8, 2, 3};
+    System.out.println(linearSearch(intArray, 2));
     
-    int[] intArray2 = { 2, 6, 78, 234, 456, 767 };
-    System.out.println( binarySearch(intArray2, 767 ) );
-    
+    int[] intArray2 = {2, 6, 78, 234, 456, 767};
+    System.out.println(binarySearch(intArray2, 767 ));   
   }
   
-  public static int linearSearch( int[] array, int key )
+  public static int linearSearch(int[] array, int key)
   {
     int size = array.length;
     
-    for ( int i = 0; i < size; i++ )
+    for (int i = 0; i < size; i++)
     {
-      if ( array[i] == key )
+      if (array[i] == key)
       {
         return i;
       }
     }
-    // 0
-    // 1
-    return -1;
+    return -1; // couldn't find key
   }
   
-  public static int binarySearch( int[] array, int key )
+  public static int binarySearch(int[] array, int key)
   {
     int low = 0;
     int high = array.length - 1;
     
-    while ( low <= high ) // more elements to search
+    while (low <= high) // more elements to search
     {
       int middle = low + (high - low) / 2;
       
-      if ( key < array[middle] )
+      if (key < array[middle])
       {
         high = middle - 1;
       }
-      else if ( key > array[middle] )
+      else if (key > array[middle])
       {
         low = middle + 1;
       }
